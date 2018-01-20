8c21f87e6455e1fb0ac0448e53bf19743b451a4c
Fan Chen <fanychen@live.ca>
2018-01-20 11:27:41 -0500

    play screen

--- app/src/main/java/josephbalawejder/ca/scavengerhunt/MainActivity.java
+++ app/src/main/java/josephbalawejder/ca/scavengerhunt/MainActivity.java
@@ -5,6 +5,8 @@ import android.graphics.Bitmap;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -19,76 +21,19 @@ import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
-    private VisualRecognition vrClient;
-    private CameraHelper helper;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Initialize Visual Recognition client
-        vrClient = new VisualRecognition(
-                VisualRecognition.VERSION_DATE_2016_05_20,
-                getString(R.string.api_key)
-        );
-
-        // Initialize camera helper
-        helper = new CameraHelper(this);
-        helper.dispatchTakePictureIntent();
+        final Button button = findViewById(R.id.playButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                startActivity(new Intent(MainActivity.this, TakePicActivity.class));
+            }
+        });
     }
-
-    @Override
-    protected void onActivityResult(int requestCode,
-                                    int resultCode,
-                                    Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if(requestCode == CameraHelper.REQUEST_IMAGE_CAPTURE) {
-            final Bitmap photo = helper.getBitmap(resultCode);
-            final File photoFile = helper.getFile(resultCode);
-            ImageView preview = findViewById(R.id.preview);
-            preview.setImageBitmap(photo);
-
-            AsyncTask.execute(new Runnable() {
-                @Override
-                public void run() {
-                    VisualClassification response =
-                            vrClient.classify(
-                                    new ClassifyImagesOptions.Builder()
-                                            .images(photoFile)
-                                            .build()
-                            ).execute();
-
-                    ImageClassification classification =
-                            response.getImages().get(0);
-
-                    VisualClassifier classifier =
-                            classification.getClassifiers().get(0);
-
-                    final StringBuffer output = new StringBuffer();
-                    for(VisualClassifier.VisualClass object: classifier.getClasses()) {
-                        if(object.getScore() > 0.7f)
-                            output.append("<")
-                                    .append(object.getName())
-                                    .append("> ");
-                    }
-
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            TextView detectedObjects =
-                                    findViewById(R.id.detected_objects);
-                            detectedObjects.setText(output);
-                        }
-                    });
-                }
-            });
-        }
-
-
-    }
-
 
 
 }
