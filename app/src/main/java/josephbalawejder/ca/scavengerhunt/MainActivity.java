e06bc1518864e9b86292ff88c3172fae87061a6c
Fan Chen <fanychen@live.ca>
2018-01-19 23:55:35 -0500

    Initializing a Visual Recognition Client

--- app/src/main/java/josephbalawejder/ca/scavengerhunt/MainActivity.java
+++ app/src/main/java/josephbalawejder/ca/scavengerhunt/MainActivity.java
@@ -3,11 +3,28 @@ package josephbalawejder.ca.scavengerhunt;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.ibm.watson.developer_cloud.android.library.camera.CameraHelper;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.VisualRecognition;
+
 public class MainActivity extends AppCompatActivity {
+
+    private VisualRecognition vrClient;
+    private CameraHelper helper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialize Visual Recognition client
+        vrClient = new VisualRecognition(
+                VisualRecognition.VERSION_DATE_2016_05_20,
+                getString(R.string.api_key)
+        );
+
+        // Initialize camera helper
+        helper = new CameraHelper(this);
     }
 }
+
+
