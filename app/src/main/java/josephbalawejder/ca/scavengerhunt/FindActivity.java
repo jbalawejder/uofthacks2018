97084fe7903c22d3e4b479f8cdfb991c489439a0
Fan Chen <fanychen@live.ca>
2018-01-20 13:26:57 -0500

    find activity created

---
+++ app/src/main/java/josephbalawejder/ca/scavengerhunt/FindActivity.java
@@ -0,0 +1,24 @@
+package josephbalawejder.ca.scavengerhunt;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class FindActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find);
+
+        final Button takePicButton = findViewById(R.id.TakePicButton);
+        takePicButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                startActivity(new Intent(FindActivity.this, TakePicActivity.class));
+            }
+        });
+    }
+}
