69754188f65724523e3dbd833dad005a4c89809a
Fan Chen <fanychen@live.ca>
2018-01-20 10:37:41 -0500

    camera works kinda

--- app/src/main/java/josephbalawejder/ca/scavengerhunt/MainActivity.java
+++ app/src/main/java/josephbalawejder/ca/scavengerhunt/MainActivity.java
@@ -2,12 +2,18 @@ package josephbalawejder.ca.scavengerhunt;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.ibm.watson.developer_cloud.android.library.camera.CameraHelper;
 import com.ibm.watson.developer_cloud.visual_recognition.v3.VisualRecognition;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.model.ClassifyImagesOptions;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.model.ImageClassification;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.model.VisualClassification;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.model.VisualClassifier;
 
 import java.io.File;
 
@@ -43,8 +49,47 @@ public class MainActivity extends AppCompatActivity {
             final File photoFile = helper.getFile(resultCode);
             ImageView preview = findViewById(R.id.preview);
             preview.setImageBitmap(photo);
+
+            AsyncTask.execute(new Runnable() {
+                @Override
+                public void run() {
+                    VisualClassification response =
+                            vrClient.classify(
+                                    new ClassifyImagesOptions.Builder()
+                                            .images(photoFile)
+                                            .build()
+                            ).execute();
+
+                    ImageClassification classification =
+                            response.getImages().get(0);
+
+                    VisualClassifier classifier =
+                            classification.getClassifiers().get(0);
+
+                    final StringBuffer output = new StringBuffer();
+                    for(VisualClassifier.VisualClass object: classifier.getClasses()) {
+                        if(object.getScore() > 0.7f)
+                            output.append("<")
+                                    .append(object.getName())
+                                    .append("> ");
+                    }
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            TextView detectedObjects =
+                                    findViewById(R.id.detected_objects);
+                            detectedObjects.setText(output);
+                        }
+                    });
+                }
+            });
         }
+
+
     }
+
+
 
 }
 
